1b080955b42a983010803d1642b4a564183e8b56
Roman Sichnyi <roman@sichnyi.com>
2021-11-25 14:24:34 -0500

    implement player and camera chase

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+//#![windows_subsystem = "windows"]
 
 use macroquad::prelude::*;
 
@@ -30,6 +30,66 @@ impl Tile {
             rect: Rect::new(x.into(), y.into(), width.into(), height.into())
         }
     }
+
+    pub fn draw(&self) {
+        draw_texture(self.texture, self.rect.x, self.rect.y, WHITE);
+    }
+}
+
+pub struct Player {
+    pub texture: Texture2D,
+    pub rect: Rect,
+    pub speed: Vec2
+}
+
+impl Player {
+    pub fn new(x: u16, y: u16) -> Player {
+        let width = TILE_SIZE;
+        let height = TILE_SIZE;
+
+        let image = Image::gen_image_color(width - 4, height - 4, RED);
+        Player {
+            texture: Texture2D::from_image(&image),
+            rect: Rect::new(x.into(), y.into(), width.into(), height.into()),
+            ..Default::default()
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_texture(self.texture, self.rect.x + 2., self.rect.y + 2., WHITE);
+    }
+
+    pub fn update(&mut self, boundaries: Rect) {
+        self.rect.move_to(self.rect.point() + self.speed);
+
+        if self.rect.left() < boundaries.left() {
+            self.rect.x = boundaries.left()
+        }
+
+        if self.rect.top() < boundaries.top() {
+            self.rect.y = boundaries.top()
+        }
+
+        if self.rect.right() > boundaries.right() {
+            self.rect.x = boundaries.right() - self.rect.w
+        }
+
+        if self.rect.bottom() > boundaries.bottom() {
+            self.rect.y = boundaries.bottom() - self.rect.h
+        }
+
+        self.speed = vec2(0., 0.);
+    }
+}
+
+impl Default for Player {
+    fn default() -> Player {
+        Player {
+            texture: Texture2D::empty(),
+            rect: Rect::new(0., 0., 0., 0.),
+            speed: vec2(0., 0.)
+        }
+    }
 }
 
 pub struct PlayerCamera {
@@ -53,92 +113,116 @@ impl PlayerCamera {
         }
     }
 
-    pub fn update(&mut self, max_x: f32, max_y: f32) {
+    pub fn update(&mut self, boundaries: Rect) {
         self.camera.target += self.speed;
-        let camera_pos = (
+        let camera_rect = (
             self.camera.screen_to_world(vec2(0., 0.)), 
             self.camera.screen_to_world(vec2(screen_width(), screen_height()))
         );
 
-        if camera_pos.0.x < 0. {
-            self.camera.target.x = self.width / 2.
+        if camera_rect.0.x < boundaries.left() {
+            self.camera.target.x = boundaries.left() + self.width / 2.
         }
         
-        if camera_pos.0.y < 0. {
-            self.camera.target.y = self.height / 2.
+        if camera_rect.0.y < boundaries.top() {
+            self.camera.target.y = boundaries.top() + self.height / 2.
         }
 
-        if camera_pos.1.x > max_x {
-            self.camera.target.x = max_x - self.width / 2.
+        if camera_rect.1.x > boundaries.right() {
+            self.camera.target.x = boundaries.right() - self.width / 2.
         }
-        
-        if camera_pos.1.y > max_y {
-            self.camera.target.y = max_y - self.height / 2.
+
+        if camera_rect.1.y > boundaries.bottom() {
+            self.camera.target.y = boundaries.bottom() - self.height / 2.
         }
     }
 }
 
 pub struct World {
     pub tiles: Vec<Tile>,
+    player: Player,
     player_camera: PlayerCamera,
-    width: f32,
-    height: f32
+    boundaries: Rect
 }
 
 impl World {
     pub fn new(map_data: Vec<Vec<i32>>) -> World {
         let mut tiles = vec![];
+        let mut player = Player::new(0, 0);
 
         for (row_idx, l) in map_data.iter().enumerate() {
             for (col_idx, x) in l.iter().enumerate() {
                 if *x == 1 {
                     tiles.push(Tile::new(col_idx as u16 * TILE_SIZE, row_idx as u16 * TILE_SIZE));
                 }
+                if *x == 2 {
+                    player.rect.x = (col_idx as u16 * TILE_SIZE).into();
+                    player.rect.y = (row_idx as u16 * TILE_SIZE).into();
+                }
             }
         }
 
         World {
             tiles,
-            width: (map_data[0].len() as u16 * TILE_SIZE) as f32,
-            height: (map_data.len() as u16 * TILE_SIZE) as f32,
+            boundaries: Rect::new(
+                0.,
+                0.,
+                (map_data[0].len() as u16 * TILE_SIZE) as f32,
+                (map_data.len() as u16 * TILE_SIZE) as f32
+            ),
+            player,
             player_camera: PlayerCamera::new(1280., 720.)
         }
     }
 
     pub fn update(&mut self) {
         for tile in self.tiles.iter() {
-            draw_tile(tile);
+            tile.draw();
         }
-        self.update_camera()
+        self.update_player();
+        self.update_camera();
+    }
+
+    pub fn update_player(&mut self) {
+        let player_speed = 4.;
+
+        if is_key_down(KeyCode::Right) {
+            self.player.speed.x = player_speed
+        }
+        if is_key_down(KeyCode::Left) {
+            self.player.speed.x = -player_speed
+        }
+        if is_key_down(KeyCode::Down) {
+            self.player.speed.y = player_speed
+        }
+        if is_key_down(KeyCode::Up) {
+            self.player.speed.y = -player_speed
+        }
+        self.player.update(self.boundaries);
+
+        self.player.draw();
     }
 
     pub fn update_camera(&mut self) {
-        
-        let mut camera_speed = vec2(0., 0.);
 
-        if is_key_down(KeyCode::Right) {
-            camera_speed.x = 4.
-        }
-        if is_key_down(KeyCode::Left) {
-            camera_speed.x = -4.
-        }
-        if is_key_down(KeyCode::Down) {
-            camera_speed.y = 4.
-        }
-        if is_key_down(KeyCode::Up) {
-            camera_speed.y = -4.
+        let target = self.player.rect.point() + self.player.rect.size() / 2.;
+        // println!("target: {}, {}, camera: {}, {}", target.x, target.y, self.player_camera.camera.target.x, self.player_camera.camera.target.y);
+
+        let offset = vec2(
+            target.x - self.player_camera.camera.target.x,
+            target.y - self.player_camera.camera.target.y
+        );
+
+        if offset.length() < 1. / 16. {
+            self.player_camera.camera.target = target
+        } else {
+            self.player_camera.speed = offset / 32.;
         }
 
-        self.player_camera.speed = camera_speed;
-
-        self.player_camera.update(self.width, self.height);
+        self.player_camera.update(self.boundaries);
 
         set_camera(&self.player_camera.camera);
     }
-}
-
-fn draw_tile(t: &Tile) {
-    draw_texture(t.texture, t.rect.x, t.rect.y, WHITE);
 }
 
 #[macroquad::main(window_conf)]
@@ -147,7 +231,7 @@ async fn main() {
     let level_map = vec![
         vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         vec![1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -167,7 +251,7 @@ async fn main() {
 
         clear_background(BLACK);
 
-        draw_text_ex("Use arrows to move camera", 100.0, 40.0, TextParams::default());
+        draw_text_ex("Use arrows to move player", 100.0, 40.0, TextParams::default());
 
         world.update();
 
